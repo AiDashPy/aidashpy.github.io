e3e21e67e4a18fd7d73d7327688bab2b4cf06026
AiDashPy <aidashpy@yahoo.com>
2024-11-20 12:52:09 -0500

    img changes

--- src/components/bookData.js
+++ src/components/bookData.js
@@ -2,55 +2,55 @@ export const bookEntries = [{
     name: "From Rebel To Ruler",
     author: "Tony Saich",
     finish: "Started 11/10/2024",
-    img: "./src/assets/covers/rebel2ruler.webp"
+    img: "https://www.hup.harvard.edu/img/feeds/jackets/9780674293915.png?fm=jpg&q=80&fit=max&w=1200"
 }, {
     name: "How To Hide An Empire",
     author: "Daniel Immerwahr",
     finish: "Finished 11/09/2024",
-    img: "./src/assets/covers/how2hide.webp"
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1627757585i/40121985.jpg"
 }, {
     name: "The Hundred Years' War on Palestine",
     author: "Rashid Khalidi",
     finish: "Finished 10/29/2024",
-    img: "./src/assets/covers/hundred.webp"
+    img: "https://mpd-biblio-covers.imgix.net/9781627798556.jpg?w=900dpr=1"
 }, {
     name: "Washington Bullets",
     author: "Vijay Prashad",
     finish: "Finished 10/26/2024",
-    img: "./src/assets/covers/washington.webp"
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1688675066i/54169138.jpg"
 }, {
     name: "Abolish Rent",
     author: "Tracy Rosenthal & Leonardo Vilchis",
     finish: "Finished 10/20/2024",
-    img: "./src/assets/covers/abolishrent.webp"
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1718715847i/205900042.jpg"
 }, {
     name: "Western Marxism",
     author: "Domenico Losurdo",
     finish: "Finished 10/14/2024",
-    img: "./src/assets/covers/westernmarx.webp"
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1719720774i/200300090.jpg"
 }, {
     name: "The Wretched Of The Earth",
     author: "Frantz Fanon",
     finish: "Finished 07/09/2024",
-    img: "./src/assets/covers/wretched.webp"
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1704228247i/66933.jpg"
 }, {
     name: "Roadside Picnic",
     author: "Arkady & Boris Strugatsky",
     finish: "Finished 07/03/2024",
-    img: "./src/assets/covers/roadside.webp"
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1173812259i/331256.jpg"
 }, {
     name: "Solaris",
     author: "Stanisław Lem",
     finish: "Finished 06/17/2024",
-    img: "./src/assets/covers/solaris.webp"
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1498631519i/95558.jpg"
 }, {
     name: "Technofeudalism: What Killed Capitalism",
     author: "Yanis Varoufakis",
     finish: "Finished 05/19/2024",
-    img: "./src/assets/covers/technof.webp"
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1684846614i/75560036.jpg"
 }, {
     name: "Liberalism: A Counter-History",
     author: "Domenico Losurdo",
     finish: "Finished 05/16/2024",
-    img: "./src/assets/covers/liberalism.webp"
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1373998827i/9670246.jpg"
 }];
