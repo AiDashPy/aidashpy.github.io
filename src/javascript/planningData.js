643b76b9fe2e0d9a6f1e2abbcbb210dbbab5d804
Aidashpy <aidashpy@yahoo.com>
2024-12-28 23:06:04 -0500

    finished gilgamesh started ilida

--- src/javascript/planningData.js
+++ src/javascript/planningData.js
@@ -30,12 +30,6 @@ export const planningEntries = [
         img: "./images/NewTheRiseAndFallOfTheItalianCommunistParty.webp"
     },
     {
-        name: "The Iliad",
-        author: "Homer",
-        finish: "Started 12/23/2024",
-        img: "./images/NewIliad.webp"
-    },
-    {
         name: "The Odyssey",
         author: "Homer",
         finish: "Planning",
