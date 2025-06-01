752e8f00660aa369467fd4214a3becf8013790fd
Avery Benfield <avery@Averys-Mac-mini.local>
2025-06-01 11:19:25 -0400

    Update WebFooter.vue

--- src/components/WebFooter.vue
+++ src/components/WebFooter.vue
@@ -8,7 +8,6 @@ import GoodreadsSearcher from './GoodreadsSearcher.vue';
         <div class="bg-gruvbox-bgS py-2 border-t border-gruvboxDark-fg2">
             <div class="flex flex-row text-gruvbox-red2 justify-center">
                 <a class="text-sm underline mx-2" href="https://www.goodreads.com/review/list/168293432?ref=nav_mybooks" target="_blank" rel="noopener noreferrer">goodreads</a>
-                <a class="text-sm underline mx-2" href="https://github.com/codespaces/turbo-eureka-5g4x6v5qvw7qhvwr7" target="_blank" rel="noopener noreferrer">codespace</a>
                 <RouterLink to="/links" class="text-sm underline mx-2" target="_blank">links</RouterLink>
                 <!-- <GoodreadsSearcher/> -->
             </div>
