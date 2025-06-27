0088fee1b7bc73b386cca29c9ef041850ca86417
Avery Benfield <avery@Averys-Mac-mini.local>
2025-06-27 12:43:57 -0400

    formatting fixes

--- src/components/BookEntry.vue
+++ src/components/BookEntry.vue
@@ -18,29 +18,30 @@ defineProps({
 </script>
 
 <template>
-    <div class="w-full text-gruvbox-gray md:text-base text-sm">
-        <div class="flex flex-row bg-gruvbox-bgS rounded-2xl border border-gruvboxDark-fg2">
-            <div class="mx-2 py-2 rounded-2xl align-middle m-auto">
-                <slot/>
+    <div
+        class="flex flex-row bg-gruvbox-bgS rounded-2xl border border-gruvboxDark-fg2 drop-shadow h-40"
+    >
+        <slot />
+
+        <div
+            class="w-full flex flex-col my-2 mr-2 px-2 pb-2 pt-1 rounded-2xl justify-between"
+        >
+            <div id="title" class="font-semibold pb-2 text-gruvbox-blue2">
+                <h1 class="inline">{{ title }}</h1>
+                <h2 class="inline"><br /></h2>
+                <h1 class="inline lowercase font-normal text-gruvbox-blue">
+                    {{ author }}
+                </h1>
+                <!-- <h2 class="inline"> - </h2> -->
+                <!-- <h2 class="inline font-normal">★★★★☆</h2> -->
+                <!-- <p class="font-normal text-sm indent-2">{{ author }}</p> -->
             </div>
+            <div id="rating" class=""></div>
             <div
-                class="w-full flex flex-col my-2 mr-2 px-2 pb-2 pt-1 rounded-2xl justify-between"
+                id="comments"
+                class="h-10 bg-gruvboxDark-fg rounded px-2 flex text-gruvbox-green2"
             >
-                <div id="title" class="font-semibold pb-2 text-gruvbox-blue2">
-                    <h1 class="inline">{{ title }}</h1>
-                    <h2 class="inline"><br /></h2>
-                    <h1 class="inline lowercase font-normal text-gruvbox-blue">{{ author }}</h1>
-                    <!-- <h2 class="inline"> - </h2> -->
-                    <!-- <h2 class="inline font-normal">★★★★☆</h2> -->
-                    <!-- <p class="font-normal text-sm indent-2">{{ author }}</p> -->
-                </div>
-                <div id="rating" class=""></div>
-                <div
-                    id="comments"
-                    class="h-10 bg-gruvboxDark-fg rounded px-2 flex text-gruvbox-green2"
-                >
-                    <p class="my-auto lowercase">{{ comment }}</p>
-                </div>
+                <p class="my-auto lowercase">{{ comment }}</p>
             </div>
         </div>
     </div>
