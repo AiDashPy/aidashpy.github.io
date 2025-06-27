cc90b1b5990ffc074d4c9b2b7625de5d2387af34
Avery Benfield <avery@Averys-Mac-mini.local>
2025-06-27 12:55:54 -0400

    Update Planning.vue

--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -1,48 +1,52 @@
 <script setup>
-    import BookEntry from "../components/BookEntry.vue";
-    import { planningEntries } from "../javascript/planningData.js";
-    import WebHeader from "../components/WebHeader.vue";
-    import WebFooter from "../components/WebFooter.vue";
+import BookEntry from "../components/BookEntry.vue";
+import { planningEntries } from "../javascript/planningData.js";
+import WebHeader from "../components/WebHeader.vue";
+import WebFooter from "../components/WebFooter.vue";
 </script>
 
 <script>
-    export default {
-        data() {
-            return {
-                bookData: planningEntries,
-            }
-        }
-    }
+export default {
+    data() {
+        return {
+            bookData: planningEntries,
+        };
+    },
+};
 </script>
 
 <template>
-
     <div class="min-h-screen bg-gruvbox-bg">
-
         <div class="min-h-screen">
-
             <WebHeader>
                 <div class="md:block hidden">
-                    <h1 v-if="index == selectedYearIndex" class="mr-2 text-gruvbox-green2 text-xl pl-12 mt-1 select-none">Planning</h1>
+                    <h1
+                        v-if="index == selectedYearIndex"
+                        class="mr-2 text-gruvbox-green2 text-xl pl-12 mt-1 select-none"
+                    >
+                        Planning
+                    </h1>
                 </div>
             </WebHeader>
 
             <div class="mx-2 xl:mx-56 shrink grow">
-
-                <div class="py-6 rounded-2xl w-full grid grid-cols-1 gap-4 md:grid-cols-2">
-                    <BookEntry class="drop-shadow" v-for="book in bookData" :title="book.name" :author="book.author" :comment="book.finish"><img class="max-h-40 rounded-xl border border-gruvboxDark-fg2" :src="book.img" :alt="book.name"></BookEntry>
+                <div
+                    class="py-6 w-full grid grid-cols-1 grid-flow-row md:grid-cols-2 gap-4"
+                >
+                    <BookEntry
+                        v-for="book in bookData"
+                        :title="book.name"
+                        :author="book.author"
+                        :comment="book.finish"
+                        ><img
+                            class="rounded-xl m-2 border border-gruvboxDark-fg2"
+                            :src="book.img"
+                            :alt="book.name"
+                    /></BookEntry>
                 </div>
-
             </div>
-
-
         </div>
 
-        <WebFooter/>
-
-
+        <WebFooter />
     </div>
-
-
-
 </template>
