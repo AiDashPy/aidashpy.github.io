9b50a22088a002a673e938a3a43a8900e240f522
Avery Benfield <avery@Averys-Mac-mini.local>
2025-06-28 12:12:14 -0400

    new formatting; new color scheme

--- src/components/WebHeader.vue
+++ src/components/WebHeader.vue
@@ -1,16 +1,27 @@
 <script setup>
-    import { RouterLink } from "vue-router";
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
-    <header id="nav" class="py-4 bg-gruvbox-bgS w-full border-b border-gruvboxDark-fg2 lowercase">
+    <header
+        id="nav"
+        class="py-4 bg-gruvboxDark-bg w-full border-b border-gruvboxDark-bgH lowercase"
+    >
         <div
             class="mx-4 xl:mx-56 flex flex-row font-mono text-2xl justify-between align-middle"
         >
-            <RouterLink to="/"><h1 class="select-none text-gruvbox-red2">Aidashpy</h1></RouterLink>
+            <RouterLink to="/"
+                ><h1 class="select-none text-gruvbox-red">
+                    Aidashpy
+                </h1></RouterLink
+            >
             <!-- <h1 class="select-none text-gruvbox-orange2 ">---</h1> -->
-            <slot/>
-            <RouterLink to="/planning"><h1 class="select-none text-gruvbox-blue2">Reading Log</h1></RouterLink>
+            <slot />
+            <RouterLink to="/planning"
+                ><h1 class="select-none text-gruvbox-blue">
+                    Reading Log
+                </h1></RouterLink
+            >
         </div>
     </header>
 </template>
--- src/javascript/planningData.js
+++ src/javascript/planningData.js
@@ -1,44 +1,44 @@
 export const planningEntries = [
-    {
-        name: "Capital - Volume 3",
-        author: "Karl Marx",
-        finish: "Planning",
-        img: "./images/NewCapitalVol3.webp"
-    },
-    {
-        name: "Grundrisse",
-        author: "Karl Marx",
-        finish: "Planning",
-        img: "./images/NewGrundrisse.webp"
-    },
-    {
-        name: "The Divine Comedy",
-        author: "Dante Alighieri",
-        finish: "Planning",
-        img: "./images/NewTheDivineComedy.webp"
-    },
-    {
-        name: "The Rise And Fall Of The Italian Communist Party",
-        author: "Silvio Pons",
-        finish: "Planning",
-        img: "./images/NewTheRiseAndFallOfTheItalianCommunistParty.webp"
-    },
-    {
-        name: "What Is To Be Done",
-        author: "Nikolay Chernyshevsky",
-        finish: "Planning",
-        img: './images/NewWhatIsToBeDone.webp'
-    },
-    {
-        name: "Don Quixote",
-        author: "Miguel De Cervantes Saavedra",
-        finish: "Planning",
-        img: "./images/NewDonQuixote.webp"
-    },
-    {
-        name: "The Class Struggle In The Ancient Greek World",
-        author: "G. E. M. DE STE. CROIX",
-        finish: "Planning",
-        img: "./images/TheClassStruggleInTheAncientGreekWorld.webp"
-    }
+  {
+    name: "Capital - Volume 3",
+    author: "Karl Marx",
+    finish: "Planning",
+    img: "./images/NewCapitalVol3.webp",
+  },
+  {
+    name: "Grundrisse",
+    author: "Karl Marx",
+    finish: "Planning",
+    img: "./images/NewGrundrisse.webp",
+  },
+  {
+    name: "The Divine Comedy",
+    author: "Dante Alighieri",
+    finish: "Planning",
+    img: "./images/NewTheDivineComedy.webp",
+  },
+  {
+    name: "The Rise And Fall Of The Italian Communist Party",
+    author: "Silvio Pons",
+    finish: "Planning",
+    img: "./images/NewTheRiseAndFallOfTheItalianCommunistParty.webp",
+  },
+  {
+    name: "What Is To Be Done",
+    author: "Nikolay Chernyshevsky",
+    finish: "Planning",
+    img: "./images/NewWhatIsToBeDone.webp",
+  },
+  {
+    name: "Don Quixote",
+    author: "Miguel De Cervantes Saavedra",
+    finish: "Planning",
+    img: "./images/NewDonQuixote.webp",
+  },
+  {
+    name: "The Class Struggle In The Ancient Greek World",
+    author: "G. E. M. DE STE. CROIX",
+    finish: "Planning",
+    img: "./images/TheClassStruggleInTheAncientGreekWorld.webp",
+  },
 ];
