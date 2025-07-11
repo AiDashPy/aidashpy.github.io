937708a11a3c9545f31b865feeba28ae5c25f703
Avery Benfield <avery@Averys-Mac-mini.local>
2025-07-10 22:43:58 -0400

    Update yearlyBookData.js

--- src/javascript/yearlyBookData.js
+++ src/javascript/yearlyBookData.js
@@ -3,6 +3,12 @@ export const yearlyBookEntries = [
     year: "2025",
     entries: [
       {
+        name: "Prelude to Revolution",
+        author: "Alexander Rabinowitch",
+        finish: "Finished 07/10/2025",
+        img: "./images/preludeToRevolution.webp",
+      },
+      {
         name: "The Soviet Experiment",
         author: "Ronald Suny",
         finish: "Finished 07/07/2025",
