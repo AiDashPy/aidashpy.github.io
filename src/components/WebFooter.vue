43a362a723c2b2cde735d9903c09f6352775f967
AiDashPy <aidashpy@yahoo.com>
2025-07-17 23:09:14 -0400

    simplifications

--- src/components/WebFooter.vue
+++ src/components/WebFooter.vue
@@ -1,26 +1,13 @@
 <script setup>
-import { RouterLink } from "vue-router";
-import GoodreadsSearcher from "./GoodreadsSearcher.vue";
+    import { RouterLink } from "vue-router";
 </script>
 
 <template>
     <footer>
         <div class="bg-gruvbox-dark-bg py-2 border-t border-gruvbox-dark-bg-h">
             <div class="flex flex-row text-gruvbox-red justify-center">
-                <a
-                    class="text-sm underline mx-2"
-                    href="https://www.goodreads.com/review/list/168293432?ref=nav_mybooks"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >goodreads</a
-                >
-                <RouterLink
-                    to="/links"
-                    class="text-sm underline mx-2"
-                    target="_blank"
-                    >links</RouterLink
-                >
-                <!-- <GoodreadsSearcher/> -->
+                <a class="text-sm underline mx-2" href="https://www.goodreads.com/review/list/168293432?ref=nav_mybooks" target="_blank" rel="noopener noreferrer" >goodreads</a>
+                <RouterLink to="/links" class="text-sm underline mx-2" target="_blank" >links</RouterLink>
             </div>
         </div>
     </footer>
--- src/views/General.vue
+++ src/views/General.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { RouterLink } from "vue-router";
+    import { RouterLink } from "vue-router";
 </script>
 
 <script>
@@ -16,48 +16,19 @@ export default {
 </script>
 
 <template>
-    <div
-        class="lowercase font-mono flex h-dvh bg-cover bg-top"
-        style="background-image: url(&quot;images/backgroundnew.webp&quot;)"
-    >
-        <Transition
-            enter-active-class="duration-1000 ease-out"
-            enter-from-class="transform opacity-0"
-            enter-to-class="opacity-100"
-            leave-active-class="duration-1000 ease-in"
-            leave-from-class="opacity-100"
-            leave-to-class="transform opacity-0"
-        >
-            <div
-                v-if="created"
-                class="border border-gruvbox-dark-fg2 shadow-xl shadow-gruvbox-dark-bg bg-gruvbox-bg1 rounded-2xl flex flex-col my-auto md:mx-auto py-4 mx-2 md:w-1/2"
-            >
-                <h1 class="text-gruvbox-dark-bg mx-auto select-none text-4xl">
-                    aidashpy
-                </h1>
+    <div class="lowercase px-2 font-mono flex h-dvh bg-cover bg-top" style="background-image: url(&quot;images/backgroundnew.webp&quot;)">
+        <Transition enter-active-class="duration-1000 ease-out" enter-from-class="transform opacity-0" enter-to-class="opacity-100" leave-active-class="duration-1000 ease-in" leave-from-class="opacity-100" leave-to-class="transform opacity-0">
+            <div v-if="created" class="border border-gruvbox-dark-fg2 shadow-xl shadow-gruvbox-dark-bg bg-gruvbox-bg1 rounded-2xl flex flex-col my-auto md:mx-auto py-4 md:w-256" >
+                <h1 class="text-gruvbox-dark-bg mx-auto select-none text-4xl">aidashpy</h1>
 
-                <a
-                    href="https://www.culture.ru/persons/9593/konstantin-yuon"
-                    target="_blank"
-                    class="w-5/6 mx-auto my-4"
-                >
-                    <img
-                        class="w-full"
-                        src="/images/ThePaintingNewPlanetNew.webp"
-                        alt="New Planet, Konstantin Yuon, 1921"
-                    />
+                <a href="https://www.culture.ru/persons/9593/konstantin-yuon" target="_blank" class="w-5/6 mx-auto my-4" >
+                    <img class="w-full" src="/images/ThePaintingNewPlanetNew.webp" alt="New Planet, Konstantin Yuon, 1921" />
                 </a>
 
-                <div
-                    class="text-gruvbox-dark-bg-s md:text-lg flex flex-row justify-center gap-4 md:gap-16 mx-10"
-                >
+                <div class="text-gruvbox-dark-bg-s md:text-lg flex flex-row justify-center gap-4 md:gap-16 mx-10" >
                     <RouterLink to="/" target="_blank">reading</RouterLink>
-                    <a href="https://letterboxd.com/aidashpy/" target="_blank"
-                        >letterboxd</a
-                    >
-                    <a href="https://anilist.co/user/Aidashpy/" target="_blank"
-                        >anilist</a
-                    >
+                    <a href="https://letterboxd.com/aidashpy/" target="_blank">letterboxd</a>
+                    <a href="https://anilist.co/user/Aidashpy/" target="_blank">anilist</a>
                 </div>
             </div>
         </Transition>
